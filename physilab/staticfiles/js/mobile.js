b8d7d67e7631b1cd259645a2b6fd9f58cce8de02
naomipricillaa <naaooommiii16@gmail.com>
2024-11-15 18:55:11 +0700

    deploy kesekian

--- physilab/staticfiles/js/mobile.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.mobile-menu-btn').addEventListener('click', function () {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
-});
