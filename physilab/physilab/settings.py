2d8c6b9b01611856fc01de51a29b5df68bb7d759
naomipricillaa <naaooommiii16@gmail.com>
2024-11-16 14:25:10 +0700

    last try vercel still error

--- physilab/physilab/settings.py
+++ physilab/physilab/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-glm(h_wbg_cx9ge@!_fbz@c3@zt9q_u!eq$4tkqjo#9_$&i+tb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
 # Application definition
